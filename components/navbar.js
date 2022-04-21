1f93a19d695fb55f77805d196793ce18dcd54264
Mark Gauffin <markgauffin@gmail.com>
2022-04-21 07:53:54 -0500

    add bootstrap functionality

--- components/navbar.js
+++ components/navbar.js
@@ -3,13 +3,50 @@ import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
     return (
-        <nav className={styles.nav}>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
-            <Link href="/articles">
-                <a>Articles</a>
-            </Link>
-        </nav>
+        // <nav className={styles.nav}>
+        //     <Link href="/">
+        //         <a>Home</a>
+        //     </Link>
+        //     <Link href="/articles">
+        //         <a>Articles</a>
+        //     </Link>
+        //     <button className="btn btn-primary">Hello</button>
+        // </nav>
+        <div className="container p-3">
+            <button className="btn btn-primary m-3">KindaCode.com</button>
+            <button className="btn btn-warning m-3">Hello</button>
+
+            <div className="dropdown m-3">
+                <button
+                    className="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    id="dropdownMenuButton1"
+                    aria-expanded="false"
+                >
+                    Dropdown button
+                </button>
+                <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton1"
+                >
+                    <li>
+                        <a className="dropdown-item" href="#">
+                            Option 1
+                        </a>
+                    </li>
+                    <li>
+                        <a className="dropdown-item" href="#">
+                            Option 2
+                        </a>
+                    </li>
+                    <li>
+                        <a className="dropdown-item" href="#">
+                            Option 3
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css';
+/* ensure all pages have Bootstrap CSS */
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Layout from '../components/layout';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+    // next.js renders the app on both the server-side and client-side
+    // there is no 'window, document' on the server side so will get errors
+    // make sure that 'window' and 'document' objects can only be used on the client side
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap');
+    }, []);
     return (
         <Layout>
             <Component {...pageProps} />
